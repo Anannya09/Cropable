f4bc323e82a8bf5711779a69bf88555c9a129ea0
Anannya09 <79738177+Anannya09@users.noreply.github.com>
2022-11-11 00:19:37 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,33 @@
-from django.urls import re_path
+"""website URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import re_path, include
+# from django.conf import settings
 
-app_name = "account"
+from detector.views import home
 
 urlpatterns = [
-    re_path(r"^signup/$", views.user_signup, name="user_signup"),
-    re_path(r"^login/$", views.user_login, name="user_login"),
-    re_path(r"^logout/$", views.user_logout, name="user_logout"),
+    re_path(r"^$", home, name="home"),
+    re_path('^admin/', admin.site.urls),
+    re_path("^user/", include("account.urls")),
+    re_path("^detect/", include("detector.urls")),
+    re_path("^get_info/", include("info.urls")),
 ]
+
+# urlpatterns += [
+#     re_path(r'^media/(?P<path>.*)$', django.views.static.serve, {
+#     'document_root': settings.MEDIA_ROOT}),]
